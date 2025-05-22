db9627756e5dfe2b854de79b7fe3aa6a4a167f6d
Walter Tapia <tapiaw38@gmail.com>
2025-05-21 23:57:55 -0300

    Add ci cd

--- vitest.config.ts
+++ vitest.config.ts
@@ -12,5 +12,16 @@ export default mergeConfig(
       exclude: [...configDefaults.exclude, 'e2e/**'],
       setupFiles: ['./tests/setup/vitest.setup.ts'],
     },
+    build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      rollupOptions: {
+        output: {
+          entryFileNames: 'assets/[name].[hash].js',
+          chunkFileNames: 'assets/[name].[hash].js',
+          assetFileNames: 'assets/[name].[hash].[ext]',
+        },
+      },
+    },
   }),
 )
