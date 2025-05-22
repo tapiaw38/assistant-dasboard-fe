b26e4be2e52fc4fb5c6409d932ece87a1811c70c
Walter Tapia <tapiaw38@gmail.com>
2025-05-22 12:55:56 -0300

    Add wsp view and services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,16 +1,19 @@
 import { authService } from './auth'
 import { assistantService } from './assistant'
 import { conversationService } from './conversation'
+import { chatflowService } from './chatflow'
 
 class Services {
   authService: typeof authService
   assistantService: typeof assistantService
   conversationService: typeof conversationService
+  chatflowService: typeof chatflowService
 
   constructor() {
     this.authService = authService
     this.assistantService = assistantService
     this.conversationService = conversationService
+    this.chatflowService = chatflowService
   }
 }
 
